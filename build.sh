47dc10299d081952b68bae9e15e0ffac164ecfe7
PersistentCloud <steven_github@jacon.cloud>
2024-07-03 20:54:30 +0200

    Adjust build script

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-DEFAULT_REGISTRY="docker.io"
-
 REGISTRY=$1
 
 if [[ $1 == "" ]]; then
-  REGISTRY=$DEFAULT_REGISTRY;
+  REGISTRY=""; else
+  REGISTRY="$1/";
 fi;
 
 echo "Used registry: ${REGISTRY}";
@@ -19,11 +18,11 @@ echo "Setting docker tag...";
 IMAGE_ID=$(docker images -q | head -n 1);
 IMAGE_REPOSITORY=$(docker images --format "{{.Repository}}" | head -n 1);
 echo $IMAGE_REPOSITORY
-docker tag $IMAGE_ID $REGISTRY/$IMAGE_REPOSITORY:latest;
-docker tag $IMAGE_ID $REGISTRY/$IMAGE_REPOSITORY:$VERSION;
-echo "Tag $REGISTRY/$IMAGE_REPOSITORY:latest set";
-echo "Tag $REGISTRY/$IMAGE_REPOSITORY:$VERSION set";
+docker tag $IMAGE_ID $REGISTRY$IMAGE_REPOSITORY:latest;
+docker tag $IMAGE_ID $REGISTRY$IMAGE_REPOSITORY:$VERSION;
+echo "Tag $REGISTRY$IMAGE_REPOSITORY:latest set";
+echo "Tag $REGISTRY$IMAGE_REPOSITORY:$VERSION set";
 
 echo "Pushing docker image...";
-docker push $REGISTRY/$IMAGE_REPOSITORY:$VERSION
-docker push $REGISTRY/$IMAGE_REPOSITORY:latest;
+docker push $REGISTRY$IMAGE_REPOSITORY:$VERSION
+docker push $REGISTRY$IMAGE_REPOSITORY:latest;
